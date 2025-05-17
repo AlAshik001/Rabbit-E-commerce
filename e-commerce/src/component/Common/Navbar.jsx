36632cd28f96fc8836ea9cfdc7786658a39367dd
AlAshik001 <alashik0009@gmail.com>
2025-05-18 03:33:48 +0530

    debuging navbar

--- e-commerce/src/component/Common/Navbar.jsx
+++ e-commerce/src/component/Common/Navbar.jsx
@@ -36,8 +36,8 @@ const Navbar = () => {
     </div>
     {/* Center Navigation Link*/}
     <div className="hidden md:flex space-x-6">
-      <Link to="/collections/all?grnder= Men" className="text-gray-700 hover:text-black text-sm font-medium uppercase">Men</Link>
-      <Link to="/collections/all?grnder= Womenr" className="text-gray-700 hover:text-black text-sm font-medium uppercase">Women</Link>
+      <Link to="/collections/all?gender=Men" className="text-gray-700 hover:text-black text-sm font-medium uppercase">Men</Link>
+      <Link to="/collections/all?gender=Women" className="text-gray-700 hover:text-black text-sm font-medium uppercase">Women</Link>
       <Link to="/collections/all?category=Top Wear" className="text-gray-700 hover:text-black text-sm font-medium uppercase">Top Wear</Link>
       <Link to="/collections/all?categoryr=Bottom Wear" className="text-gray-700 hover:text-black text-sm font-medium uppercase">Bottom wear</Link>
     </div>
